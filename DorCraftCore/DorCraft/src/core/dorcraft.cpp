5dfb4a6a3cfea087b70238173dcf30c41678aa9e
Doringus <kekenken3120@gmail.com>
2020-09-25 00:40:06 +0600

    Added frame timing

--- DorCraftCore/DorCraft/src/core/dorcraft.cpp
+++ DorCraftCore/DorCraft/src/core/dorcraft.cpp
@@ -7,6 +7,11 @@ static void processGlfwButtonInput(GLFWwindow *window , gameButtonState_t* newSt
 }
 
 void gameLoop(GLFWwindow *window) {
+	double timePerFrame = 1.0 / 60.0;
+	double timer = glfwGetTime();
+	double dt = 0.0;
+	double lastTime = glfwGetTime();
+	short updates = 0, frames = 0;
 	while (!glfwWindowShouldClose(window)) {
 		/* Input */
 		gameInput_t input = {};
@@ -16,7 +21,28 @@ void gameLoop(GLFWwindow *window) {
 		processGlfwButtonInput(window, &input.right, GLFW_KEY_D);
 
 		if (input.forward.pressed) {
-			printf("Key `W` was pressed");
+			printf("Key `W` was pressed\n");
+		}
+
+		double currentTime = glfwGetTime();
+		dt += (currentTime - lastTime) / timePerFrame;
+		lastTime = currentTime;
+		while (dt >= 1.0) {
+			/* Update */
+			/**********/
+			updates++;
+			dt--;
+		}
+
+		/* Render */
+
+		/***********/
+		frames++;
+		if (currentTime - timer > 1.0) {
+			timer = currentTime;
+			printf("Updates %d, frames %d\n", updates, frames);
+			updates = 0;
+			frames = 0;
 		}
 
 		glClear(GL_COLOR_BUFFER_BIT);
